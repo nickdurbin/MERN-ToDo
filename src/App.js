ea6dd88d30fe71feddfe17e034b73019723202c9
ndurbin <nick.durbin@gmail.com>
2019-05-19 08:31:02 -0400

    Fixed a few errors with spelling.

--- src/App.js
+++ src/App.js
@@ -14,9 +14,6 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
