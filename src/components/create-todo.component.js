b0479e2563d1f4c05a2dba312d8b70148f3b2a55
ndurbin <nick.durbin@gmail.com>
2019-05-19 07:58:51 -0400

    Added some functionality to the create component by giving it a current state function.

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -9,3 +9,32 @@ export default class CreateTodo extends Component {
         )
     }
 }
+
+constructor(props) {
+    super(props);
+
+    this.state = {
+        todo_description: '',
+        todo_responsible: '',
+        todo_priority: '',
+        todo_completed: false
+    }
+}
+
+onChangeTodoDescription(e) {
+    this.ListeningStateChangedEvent({
+        todo_description: e.target.value
+    });
+}
+
+onChangeTodoResponsible(e) {
+    this.ListeningStateChangedEvent({
+        todo_responsible: e.target.value
+    });
+}
+
+onChangeTodoPriority(e) {
+    this.ListeningStateChangedEvent({
+        todo_priority: e.target.value
+    });
+}
