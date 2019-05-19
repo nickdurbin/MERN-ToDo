9f666ee79269d191262a6a7d3143a621e68b242e
ndurbin <nick.durbin@gmail.com>
2019-05-19 08:40:22 -0400

    Made some minor changes.

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -2,55 +2,55 @@ import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
  
-  constructor(props) {
-    super(props);
+constructor(props) {
+  super(props);
 
-    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
-    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
-    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+  this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+  this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+  this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+  this.onSubmit = this.onSubmit.bind(this);
 
-    this.state = {
+  this.state = {
       todo_description: '',
       todo_responsible: '',
       todo_priority: '',
       todo_completed: false
-    }
   }
-  
-  onChangeTodoDescription(e) {
-    this.ListeningStateChangedEvent({
+}
+
+onChangeTodoDescription(e) {
+  this.setState({
       todo_description: e.target.value
-    });
-  }
-  
-  onChangeTodoResponsible(e) {
-    this.ListeningStateChangedEvent({
+  });
+}
+
+onChangeTodoResponsible(e) {
+  this.setState({
       todo_responsible: e.target.value
-    });
-  }
-  
-  onChangeTodoPriority(e) {
-    this.ListeningStateChangedEvent({
+  });
+}
+
+onChangeTodoPriority(e) {
+  this.setState({
       todo_priority: e.target.value
-    });
-  }
+  });
+}
+
+onSubmit(e) {
+  e.preventDefault();
   
-  onSubmit(e) {
-    e.preventDefault();
-
-    console.log(`Form submitted:`);
-    console.log(`Todo Description: ${this.state.todo_description}`);
-    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
-    console.log(`Todo Priority: ${this.state.todo_priority}`);
-
-    this.ListeningStateChangedEvent({
+  console.log(`Form submitted:`);
+  console.log(`Todo Description: ${this.state.todo_description}`);
+  console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+  console.log(`Todo Priority: ${this.state.todo_priority}`);
+  
+  this.setState({
       todo_description: '',
       todo_responsible: '',
       todo_priority: '',
       todo_completed: false
-    });
-  }        
+  })
+}       
  
 render() {
   return (
